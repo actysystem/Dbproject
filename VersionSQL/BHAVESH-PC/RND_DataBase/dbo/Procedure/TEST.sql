c09a971ff63e6d9ad3cf48577d3553f4f9b393c9
unknown <unknown>
2017-12-13 13:22:31 +0530

    update procedure

---
+++ VersionSQL/BHAVESH-PC/RND_DataBase/dbo/Procedure/TEST.sql
@@ -0,0 +1,16 @@
+/****** Object:  Procedure [dbo].[TEST]    Committed by VersionSQL https://www.versionsql.com ******/
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE TEST		
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+   select * from Employee
+END
